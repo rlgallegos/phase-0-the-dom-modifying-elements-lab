0525acdac7bf84b2dbbef9f36c573b1c2c03a97c
Robert Gallegos <rlgallegos85@gmail.com>
2023-01-13 20:20:37 -0500

    dom modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const main = document.getElementById('main');
+main.remove();
+
+//create newHeader variable and append
+const newHeader = document.createElement('h1');
+
+//input id
+newHeader.id = 'victory';
+
+//add text
+newHeader.textContent = 'Bobby is the champion';
